412733c7d1258059a45f6caff02ae452365621b9
MatiSal <salinasmatias100@gmail.com>
2023-05-30 11:59:29 -0300

    add loader in categories and categoriesBrowser

--- src/app/components/category-games/category-games.component.ts
+++ src/app/components/category-games/category-games.component.ts
@@ -1,5 +1,6 @@
 import { Component, HostListener, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { throwError } from 'rxjs';
 import { games } from 'src/app/models/games';
 import { DataService } from 'src/app/services/data.service';
 
@@ -14,31 +15,30 @@ export class CategoryGamesComponent implements OnInit {
   ini = 0;
   limit = 20
   currentPage = 0
+  flagLoader: boolean = true
 
   constructor(private _service: DataService, private rutaActiva: ActivatedRoute, private route: Router) { }
 
   ngOnInit(): void {
     this.rutaActiva.params.subscribe(params => {
       const category = params['category'];
-      // console.log(category)
-      if (category) {
-        this.getGames(category)
+      const platform = params['platform'];
+      if (category && platform) {
+        this.getGames(category, platform)
       }
     });
 
   }
 
-  getGames(category:string) {
-    const platform = 'pc'
+  getGames(category: string, platform: string) {
     this._service
       .getGamesPerCategory(category, platform)
       .subscribe({
         next: (data: games[]) => {
           this.arrayGames = data;
-          // console.log(this.arrayGames)
         },
         error: (err) => {
-          console.log(err)
+          throw (err)
         },
       });
 
@@ -46,10 +46,18 @@ export class CategoryGamesComponent implements OnInit {
 
   @HostListener('window:scroll', ['$event'])
   onScroll() {
+
     if ((document.body.clientHeight + window.scrollY + 210) >= document.body.scrollHeight) {
-      console.log('triggred');
-      this.limit = this.limit + 20
-  }
+      setTimeout(() => {
+        if (this.limit >= this.arrayGames.length) {
+          this.flagLoader = false
+        }
+        else {
+          this.limit = this.limit + 20
+        }
+
+      }, 1000);
+    }
   }
 
   goDetails(id: number) {
